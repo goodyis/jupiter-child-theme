7520a73852b7b6d5f8ffd108f046b09d0b27da3b
goodyis <goodyis@gmail.com>
2017-05-10 11:41:14 -0500

    j5-child v.0.0.1
    Jupiter-Child-Theme Init
    
    Jupiter by Artbees
    Jupiter Child Theme by @goodyis

---
+++ jupiter-child/functions.php
@@ -0,0 +1,15 @@
+<?php
+	
+
+/* MAY 2017 Jupiter Base, Customization --> @goodyis */
+
+
+require_once get_stylesheet_directory()."/base_functions.php";
+
+
+/* ANY THING YOU WOULD/COULD ADD to the functions.php can go here */
+/* ONLY edit the base_functions.php if you make addition theme customizations */
+/* Such as changing/altering shortcodes, views, components, assets etc... */
+
+
+?>
